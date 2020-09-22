9852dc5bf69d76394f1b1c1ab711563bbcf31f3c
Nicklas Millard <n.millard@me.com>
2020-09-22 16:10:09 +0200

    add guard

--- Medium.ReplacingIfElse.Application/CommandHandlers/Commands/ChangeUsernameCommand.cs
+++ Medium.ReplacingIfElse.Application/CommandHandlers/Commands/ChangeUsernameCommand.cs
@@ -1,6 +1,11 @@
+using System;
+
 namespace Medium.ReplacingIfElse.Application.CommandHandlers.Commands {
     public class ChangeUsernameCommand {
         public ChangeUsernameCommand(string email, string newUsername) {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(newUsername))
+                throw new ArgumentException();
+            
             Email = email;
             NewUsername = newUsername;
         }
